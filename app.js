08976cb8de8da71c37edbc76ab8380d4d33bfb71
sumaiya@071 <sumaiyaprvj07@gmail.com>
2024-07-04 23:22:51 +0530

    Add files

---
+++ app.js
@@ -0,0 +1,15 @@
+let getstartedBtn = document.querySelector("#get-started");
+let HomeBtn = document.querySelector("Home");
+let artCollectionBtn = document.querySelector("artCollection");
+
+
+function myFunction() {
+    location.replace("http://127.0.0.1:5500/index.html")
+}
+function myFunction1() {
+    location.replace("http://127.0.0.1:5500/artCollection.html")
+}
+function myFunction2() {
+    location.replace("http://127.0.0.1:5500/artwork.html#")
+}
+
